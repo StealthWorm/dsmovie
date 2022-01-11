6642c5d3739dd697403504a52d30b0789496ae3d
Thierry Santos <ThierryPitela@hotmail.com>
2022-01-10 23:09:06 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,39 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+
+//bootstrap: breakpoints para definir tamanho dinamicos de componentes de acordo com o tamanho da tela
+/*
+small             sm
+medium            md
+large             lg
+extra large       xl
+extra extra large xxl 
+*/
 
 function Listing() {
    return (
-      <Pagination />
+      <>
+         <Pagination />
+         <div className="container">
+            <div className="row">
+               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+                  <MovieCard />
+               </div>
+               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+                  <MovieCard />
+               </div>
+               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+                  <MovieCard />
+               </div>
+               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+                  <MovieCard />
+               </div>
+               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+                  <MovieCard />
+               </div>
+            </div>
+         </div>
+      </>
    );
 }
 
