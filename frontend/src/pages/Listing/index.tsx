1e0c2a26e5c7d6dccf18aa2b131d247012c263f8
Thierry Santos <ThierryPitela@hotmail.com>
2022-01-15 13:19:10 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from 'utils/requests';
 
 //bootstrap: breakpoints para definir tamanho dinamicos de componentes de acordo com o tamanho da tela
@@ -14,14 +16,19 @@ extra extra large xxl
 
 function Listing() {
 
-   //forma errada
-   axios.get(`${BASE_URL}/movies?size=12&page=0`)
-      .then(response => {
-         console.log(response.data);
-      });
+   const [pageNumber, setPageNumber] = useState(0);
+
+   useEffect(() => {
+      axios.get(`${BASE_URL}/movies?size=12&page=0`)
+         .then(response => {
+            const data = response.data as MoviePage;
+            setPageNumber(data.number)
+         });
+   }, []);
 
    return (
       <>
+         <p>{pageNumber}</p>
          <Pagination />
          <div className="container">
             <div className="row">
