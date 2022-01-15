00400fc378d2744a76234224d719a70a046bfed7
Thierry Santos <ThierryPitela@hotmail.com>
2022-01-15 13:02:25 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,5 +1,7 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from 'utils/requests';
 
 //bootstrap: breakpoints para definir tamanho dinamicos de componentes de acordo com o tamanho da tela
 /*
@@ -11,24 +13,31 @@ extra extra large xxl
 */
 
 function Listing() {
+
+   //forma errada
+   axios.get(`${BASE_URL}/movies?size=12&page=0`)
+      .then(response => {
+         console.log(response.data);
+      });
+
    return (
       <>
          <Pagination />
          <div className="container">
             <div className="row">
-               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                   <MovieCard />
                </div>
-               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                   <MovieCard />
                </div>
-               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                   <MovieCard />
                </div>
-               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                   <MovieCard />
                </div>
-               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                   <MovieCard />
                </div>
             </div>
